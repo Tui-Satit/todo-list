856a2a2fdc5f040e8a5b68e9bd52545558575b00
Tui <m.satit@gmail.com>
2026-06-02 16:45:29 +0700

    Improve app

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,8 @@ function App() {
 });
 
   const [newTask, setNewTask] = useState("");
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editingText, setEditingText] = useState("");
 
   const addTask = () => {
       if (!newTask.trim()) return;
@@ -52,11 +54,34 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
+  const startEdit = (index, text) => {
+    setEditingIndex(index);
+    setEditingText(text);
+  };
+
+  const saveEdit = () => {
+    if (!editingText.trim()) return;
+  
+
+  const updatedTasks = tasks.map((task, index) => 
+     index === editingIndex
+       ? {...task, text: editingText }
+       : task
+  );
+  
+
+  setTasks(updatedTasks);
+  setEditingIndex(null);
+  setEditingText("");
+};
+
   return (
     <main className="app">
       <section className="todo-card">
         <h1>My To-Do List</h1>
-
+     <p className="task-count">
+      {tasks.filter((task) => task.completed).length} / {tasks.length} completed
+     </p>
         <div className="todo-form">
           <input 
           type="text" 
@@ -77,17 +102,48 @@ function App() {
     <ul className="todo-list">
           {tasks.map((task, index) => (
           <li key={index} className={task.completed ? "completed" : ""}>
-            <span onClick={() => toggleTask(index)}>
+            {editingIndex === index ? (
+              <input
+                className="edit-input"
+                value={editingText}
+                onChange={(e) => setEditingText(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    saveEdit();
+                  }
+                }}
+                />
+            ) : (
+                <span onClick={() => toggleTask(index)}>
               {task.text}
            </span>
+            )}
 
+            <div className="task-actions">   
+              {editingIndex === index ? (
+             <button className="save-btn" onClick={saveEdit}
+            >
+              save
+            </button>
+              ) : (
+
+            <button 
+            className="edit-btn"
+            onClick={() => startEdit(index, task.text)}
+            >
+              Edit
+            </button>
+
+           )}
+         
+          
             <button 
             className="delete-btn"
             onClick={() => deleteTask(index)}
             >
               Delete
             </button>
-
+            </div> 
           </li>
         ))}
         </ul>
